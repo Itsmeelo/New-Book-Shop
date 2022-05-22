e93629a228759d8b5a0e009435c1f4a7b4197139
Itsmeelo <98872166+Itsmeelo@users.noreply.github.com>
2022-05-22 21:09:40 +0400

    feat: make changes

--- script.js
+++ script.js
@@ -196,9 +196,7 @@ getUser();*/
 
 
 fetch('books.json') //path to the file with json data 
-    .then(response => {
-        return response.json();
-    })
+    .then(response => response.json())
     .then(data => {
         console.log(data.first_title);
         document.querySelector('.pOne').innerText = data.first_title
